cd63470ee20222cbd41e1613699587ab0dd54698
formanchuk <bohdan.formanchuk@oa.edu.ua>
2026-06-18 11:43:50 +0300

    Lab 8: Blog posts output with sample data

---
+++ app/Models/appModelsPost.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'user_id',
+        'title',
+        'slug',
+        'excerpt',
+        'content',
+        'is_published',
+        'published_at',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'is_published' => 'boolean',
+            'published_at' => 'datetime',
+        ];
+    }
+}
