b850c26a1884b72ef91810be02f5db51f0f33521
formanchuk <bohdan.formanchuk@oa.edu.ua>
2026-06-24 13:48:47 +0300

    Lab 17: API Resources for Post, Category, User

---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'excerpt' => $this->excerpt,
+            'content' => $this->content,
+            'is_published' => $this->is_published,
+            'published_at' => $this->published_at?->format('Y-m-d H:i:s'),
+            'created_at' => $this->created_at?->format('Y-m-d H:i:s'),
+            'updated_at' => $this->updated_at?->format('Y-m-d H:i:s'),
+            'category' => new CategoryResource($this->whenLoaded('category')),
+            'user' => new UserResource($this->whenLoaded('user')),
+        ];
+    }
+}
